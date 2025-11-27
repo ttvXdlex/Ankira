37af12f1465cb3ec643fae042e9c1ddbad5b969b
tarav <alex.tarakanov@ivkhk.ee>
2025-11-27 15:27:14 +0200

    chut chut slomano no teper razdelno

---
+++ src/main/java/org/example/Borderpane.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Borderpane extends AnkiraInterface {
+
+    @Override
+    public void start(Stage stage) {
+
+//        BorderPane root = new BorderPane();
+//        root.setStyle("-fx-background-color: #191919");
+    }
+}
